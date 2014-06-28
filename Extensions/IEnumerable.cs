318d7a5dbe54f7a84110cdfd228e605379a511e4
Boris Scheiman <bscheiman@gmail.com>
2014-06-28 00:58:11 -0500

    RandomHelper, IEnumerable.GetRandomElement

---
+++ Extensions/IEnumerable.cs
@@ -0,0 +1,23 @@
+﻿#region
+using System.Collections.Generic;
+using System.Linq;
+using bscheiman.Common.Helpers;
+
+#endregion
+
+namespace bscheiman.Common.Extensions {
+    public static partial class Extensions {
+        /// <summary>
+        /// Returns a random element from the specified list.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list">Source list.</param>
+        /// <returns>Random element</returns>
+        public static T GetRandomElement<T>(this IEnumerable<T> list) {
+            var tmp = list.ToList();
+            var count = tmp.Count();
+
+            return count == 0 ? default(T) : tmp.ElementAt(RandomHelper.Instance.Next(count));
+        }
+    }
+}
---
+++ Helpers/RandomHelper.cs
@@ -0,0 +1,24 @@
+﻿#region
+using System;
+using System.Threading;
+
+#endregion
+
+namespace bscheiman.Common.Helpers {
+    public static class RandomHelper {
+        internal static int SeedCounter = new Random().Next();
+        [ThreadStatic] private static Random _rng;
+
+        public static Random Instance {
+            get {
+                if (_rng == null) {
+                    int seed = Interlocked.Increment(ref SeedCounter);
+
+                    _rng = new Random(seed);
+                }
+
+                return _rng;
+            }
+        }
+    }
+}
