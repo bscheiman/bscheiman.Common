70db25e64dbc8ed04354a0b9cbfa3fa3f6fd173d
Boris Scheiman <bscheiman@gmail.com>
2014-12-02 19:49:41 -0600

    DateUtil.Now

---
+++ Util/DateUtil.cs
@@ -0,0 +1,11 @@
+﻿#region
+using System;
+using bscheiman.Common.Extensions;
+
+#endregion
+
+namespace bscheiman.Common.Util {
+    public static class DateUtil {
+        public static long Now = DateTime.UtcNow.ToEpoch();
+    }
+}
