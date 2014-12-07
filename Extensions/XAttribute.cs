ec29102e87c15f5c6ba9dc1dc5b7c70a7b63b692
Boris Scheiman <bscheiman@gmail.com>
2014-12-07 12:35:52 -0600

    XAttribute, XElement

---
+++ Extensions/XAttribute.cs
@@ -0,0 +1,23 @@
+﻿#region
+using System;
+using System.Xml.Linq;
+
+#endregion
+
+namespace bscheiman.Common.Extensions {
+    public static partial class Extensions {
+        public static string ValueOrDefault(this XAttribute attr) {
+            return ValueOrDefault(attr, string.Empty);
+        }
+
+        public static T ValueOrDefault<T>(this XAttribute attr, T defaultValue) {
+            if (attr == null)
+                return defaultValue;
+
+            var str = attr.Value;
+            var type = typeof (T);
+
+            return (T) Convert.ChangeType(str, type);
+        }
+    }
+}
---
+++ Extensions/XElement.cs
@@ -0,0 +1,21 @@
+﻿#region
+using System;
+using System.Xml.Linq;
+
+#endregion
+
+namespace bscheiman.Common.Extensions {
+    public static partial class Extensions {
+        public static string ValueOrDefault(this XElement attr) {
+            return ValueOrDefault(attr, string.Empty);
+        }
+
+        public static string ValueOrDefault(this XElement attr, string defaultValue) {
+            return attr == null ? defaultValue : attr.Value;
+        }
+
+        public static T ValueOrDefault<T>(this XElement elem) {
+            return elem == null ? default(T) : (T) Convert.ChangeType(elem.Value, typeof (T));
+        }
+    }
+}
