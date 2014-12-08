df00b92a6aa8eba7a651693cf61ab5e6d16ab15f
Boris Scheiman <bscheiman@gmail.com>
2014-12-08 15:48:27 -0600

    HttpWebRequest

---
+++ Extensions/HttpWebRequest.cs
@@ -0,0 +1,26 @@
+﻿#region
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Reflection;
+
+#endregion
+
+namespace bscheiman.Common.Extensions {
+    public static partial class Extensions {
+        private static readonly string[] RestrictedHeaders = {
+            "Accept", "Connection", "Content-Length", "Content-Type", "Date", "Expect", "Host", "If-Modified-Since", "Range", "Referer",
+            "Transfer-Encoding", "User-Agent", "Proxy-Connection"
+        };
+
+        private static readonly Dictionary<string, PropertyInfo> HeaderProperties =
+            new Dictionary<string, PropertyInfo>(StringComparer.OrdinalIgnoreCase);
+
+        public static void SetRawHeader(this HttpWebRequest request, string name, string value) {
+            if (HeaderProperties.ContainsKey(name))
+                HeaderProperties[name].SetValue(request, value, null);
+            else
+                request.Headers[name] = value;
+        }
+    }
+}
