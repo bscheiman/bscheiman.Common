398ec2b6a494e0646f9de8ebe2540e98ebd9fd84
Boris Scheiman <bscheiman@gmail.com>
2014-12-27 22:29:42 -0600

    Overloads

--- Util/Logger.cs
+++ Util/Logger.cs
@@ -23,6 +23,86 @@ namespace bscheiman.Common.Util {
 
         private static bool Initialized { get; set; }
 
+        public static void Debug(string str, params object[] objs) {
+            if (!Initialized)
+                Setup();
+
+            str = string.Format("«DEBUG/{0}» {1}", Caller, string.Format(str, objs));
+
+            foreach (var l in Loggers)
+                l.Debug(str);
+        }
+
+        public static void Debug(string str) {
+            if (!Initialized)
+                Setup();
+
+            str = string.Format("«DEBUG/{0}» {1}", Caller, str);
+
+            foreach (var l in Loggers)
+                l.Debug(str);
+        }
+
+        public static void Error(string str, params object[] objs) {
+            if (!Initialized)
+                Setup();
+
+            str = string.Format("«ERROR/{0}» {1}", Caller, string.Format(str, objs));
+
+            foreach (var l in Loggers)
+                l.Error(str);
+        }
+
+        public static void Error(string str) {
+            if (!Initialized)
+                Setup();
+
+            str = string.Format("«ERROR/{0}» {1}", Caller, str);
+
+            foreach (var l in Loggers)
+                l.Error(str);
+        }
+
+        public static void Fatal(string str, Exception e) {
+            if (!Initialized)
+                Setup();
+
+            str = string.Format("«FATAL/{0}» {1}: {2}", Caller, string.Format(str, e));
+
+            foreach (var l in Loggers)
+                l.Fatal(str);
+        }
+
+        public static void Fatal(string str) {
+            if (!Initialized)
+                Setup();
+
+            str = string.Format("«FATAL/{0}» {1}: {2}", Caller, str);
+
+            foreach (var l in Loggers)
+                l.Fatal(str);
+        }
+
+        public static void Info(string str, params object[] objs) {
+            if (!Initialized)
+                Setup();
+
+            str = string.Format("«INFO/{0}» {1}", Caller, string.Format(str, objs));
+
+            foreach (var l in Loggers)
+                l.Info(str);
+        }
+
+        public static void Info(string str) {
+            if (!Initialized)
+                Setup();
+
+            str = string.Format("«INFO/{0}» {1}", Caller, str);
+
+            foreach (var l in Loggers)
+                l.Info(str);
+        }
+
         public static void Setup() {
             Setup(DefaultConfig);
         }
@@ -53,56 +133,21 @@ namespace bscheiman.Common.Util {
                 l.Teardown();
         }
 
-        [DllImport("kernel32.dll")]
-        private static extern IntPtr GetConsoleWindow();
-
-        #region Logging functions
-        public static void Debug(string str, params object[] objs) {
-            if (!Initialized)
-                Setup();
-
-            str = string.Format("«DEBUG/{0}» {1}", Caller, string.Format(str, objs));
-
-            foreach (var l in Loggers)
-                l.Debug(str);
-        }
-
-        public static void Error(string str, params object[] objs) {
-            if (!Initialized)
-                Setup();
-
-            str = string.Format("«ERROR/{0}» {1}", Caller, string.Format(str, objs));
-
-            foreach (var l in Loggers)
-                l.Error(str);
-        }
-
-        public static void Fatal(string str, Exception e) {
-            if (!Initialized)
-                Setup();
-
-            str = string.Format("«FATAL/{0}» {1}: {2}", Caller, string.Format(str, e));
-
-            foreach (var l in Loggers)
-                l.Fatal(str);
-        }
-
-        public static void Info(string str, params object[] objs) {
-            if (!Initialized)
-                Setup();
-
-            str = string.Format("«INFO/{0}» {1}", Caller, string.Format(str, objs));
-
-            foreach (var l in Loggers)
-                l.Info(str);
-        }
-
         public static void Warn(string str, params object[] objs) {
             str = string.Format("«WARN/{0}» {1}", Caller, string.Format(str, objs));
 
             foreach (var l in Loggers)
                 l.Warn(str);
         }
-        #endregion
+
+        public static void Warn(string str) {
+            str = string.Format("«WARN/{0}» {1}", Caller, str);
+
+            foreach (var l in Loggers)
+                l.Warn(str);
+        }
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
     }
 }
